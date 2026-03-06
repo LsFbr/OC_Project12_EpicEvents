10a5a7e606b9cae90e3e62ba88f731ab2e2a7ecf
LsFbr <louisfabre@hotmail.fr>
2026-03-06 15:07:22 +0100

    test(tests/conftest.py): add session fixture for in-memory SQLite database to facilitate testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,22 @@ import epicevents.models.collaborator
 import epicevents.models.role 
 
 import os
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from epicevents.models.base import Base
+from epicevents.db.session import SessionLocal
 
 # Set the JWT secret key for the tests
 os.environ["EPICEVENTS_SECRET"] = "test-secret-test-secret-test-secret"
+
+@pytest.fixture
+def session():
+    engine = create_engine("sqlite+pysqlite:///:memory:", future=True)
+    Base.metadata.create_all(engine)
+    SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+    s = SessionLocal()
+    try:
+        yield s
+    finally:
+        s.close()   
