d78aa9e825f4e63ae8867deca3fd89d1516474db
LsFbr <louisfabre@hotmail.fr>
2026-03-06 10:27:12 +0100

    chore(tests/conftest.py): set JWT secret key for testing environment

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,3 +3,8 @@ import epicevents.models.contract
 import epicevents.models.event 
 import epicevents.models.collaborator  
 import epicevents.models.role 
+
+import os
+
+# Set the JWT secret key for the tests
+os.environ["EPICEVENTS_SECRET"] = "test-secret-test-secret-test-secret"
