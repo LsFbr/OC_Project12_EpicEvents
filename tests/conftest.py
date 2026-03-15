3d7fb053c5ce58a93ec861af0c2434e5a44cfa73
LsFbr <louisfabre@hotmail.fr>
2026-03-15 15:02:35 +0100

    refactor(tests/conftest.py): remove unused model imports to clean up test configuration

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,3 @@
-import epicevents.models.client 
-import epicevents.models.contract
-import epicevents.models.event 
-import epicevents.models.collaborator  
-import epicevents.models.role 
-
 import os
 import pytest
 from sqlalchemy import create_engine
