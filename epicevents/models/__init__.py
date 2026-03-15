7e94814841f8605581b205e7247de18905a4d6e6
LsFbr <louisfabre@hotmail.fr>
2026-03-15 15:02:24 +0100

    feat(models/__init__.py): import models for role, collaborator, client, contract, and event

---
+++ epicevents/models/__init__.py
@@ -0,0 +1,5 @@
+from epicevents.models.role import Role
+from epicevents.models.collaborator import Collaborator
+from epicevents.models.client import Client
+from epicevents.models.contract import Contract
+from epicevents.models.event import Event
