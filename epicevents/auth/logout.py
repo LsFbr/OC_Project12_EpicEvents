c9984f70ea7bc312c6af84594e31358133eab47e
LsFbr <louisfabre@hotmail.fr>
2026-03-20 08:59:47 +0100

    docs(auth/logout.py): add docstring to logout function to clarify its purpose and return value

--- epicevents/auth/logout.py
+++ epicevents/auth/logout.py
@@ -1,5 +1,12 @@
 from epicevents.auth.token_storage import delete_token
 
 def logout() -> None:
+    """
+    Logout a collaborator and delete the JWT token.
+    Args:
+        None
+    Returns:
+        None
+    """
     if not delete_token():
         raise Exception("You were not logged in")
