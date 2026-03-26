f9a4428bc571c446c2f99780b95ae3f6d844f0fe
LsFbr <louisfabre@hotmail.fr>
2026-03-26 16:56:41 +0100

    refactor(test_login.py): enhance login tests by introducing fake session and user models for improved authentication handling and test clarity

--- tests/unit/test_login.py
+++ tests/unit/test_login.py
@@ -1,6 +1,8 @@
 import pytest
 
 from epicevents.auth.login import login
+from tests.conftest import FakeSession, FakeUser
+from epicevents.models.collaborator import Collaborator
 
 
 
@@ -23,9 +25,9 @@ def test_login_wrong_password(monkeypatch, fake_session):
         login("test@test.com", "password")
 
 
-def test_login_success(monkeypatch, fake_session):
+def test_login_success(monkeypatch):
 
-    monkeypatch.setattr("epicevents.auth.login.SessionLocal", lambda: fake_session)
+    monkeypatch.setattr("epicevents.auth.login.SessionLocal", lambda: FakeSession(query_map={Collaborator: [FakeUser(email="test@test.com")]}))
     monkeypatch.setattr("epicevents.auth.login.verify_password", lambda password, hash: True)
     monkeypatch.setattr("epicevents.auth.login.generate_token", lambda user: "fake-token")
 
